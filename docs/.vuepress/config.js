e6675412e821e97b451a69b72191d4bd049f9249
dianligegege <1220671124@qq.com>
2019-10-27 23:39:22 +0800

    fix: 导航栏和侧边栏

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,40 +1,56 @@
 module.exports = {
-    title: 'Hello VuePress',
-    description: 'dianligegege\'s blog',
-    base: '/',
-    head: [
-      ['link', { rel: 'shortcut icon', href:'/image/favicon.ico' }],
-    ],
-    // 默认样式配置
-    themeConfig: {
-      // 导航栏
-      nav: [
-        { text: 'Home', link: '/' },
-        { text: 'demo', link: '/demo' },
-        { text: 'github', link: 'https://github.com/dianligegege/my-blog'},
-        {
-          text: 'Languages',
-          // 网页阅读
-          ariaLabel: 'Language Menu',
-          items: [
-            { text: 'Chinese', link: '/language/chinese/' },
-            { text: 'Japanese', link: '/language/japanese/' }
-          ]
-        }
-      ],
-      // 侧边栏
-      sidebar: [
-        ['/', 'home页'],
-        ['/demo', 'demo页'],
-        ['/page', 'page页']
-      ]
-  },
-  // webpack配置
-  configureWebpack: {
-    resolve: {
-      alias: {
-        '@': './.vuepress/public'
-      }
-    }
-  },
+	title: '电里',
+	description: 'dianligegege\'s blog',
+	base: '/',
+	head: [
+		['link', { rel: 'shortcut icon', href: '/image/favicon.ico' }],
+	],
+	// 默认样式配置
+	themeConfig: {
+		// 导航栏
+		nav: [
+			{ text: 'Home', link: '/' },
+			{
+				text: '文档',
+				items: [
+					{ text: '旧文章', link: '/old/' },
+					{ text: '新文章', link: '/new/' }
+				]
+			},
+			{ text: 'github', link: 'https://github.com/dianligegege/my-blog' },
+		],
+		sidebar: {
+			'/old/': [
+				{
+					title: '一些旧文章',   // 必要的
+					path: '/old/',    // 可选的, 应该是一个绝对路径
+					collapsable: false, // 可选的, 默认值是 true,
+					sidebarDepth: 1,    // 可选的, 默认值是 1
+					children: [
+						['document', '旧文章一'],
+						['document1', '旧文章二']
+					]
+				},
+			],
+
+			'/new/': [
+				['', '新文章'],
+				['page', '新文章1'],
+				['page1', '新文章2']
+			],
+
+			// fallback
+			'/': [
+				'',        /* / */
+			]
+		}
+	},
+	// webpack配置
+	configureWebpack: {
+		resolve: {
+			alias: {
+				'@': '../.vuepress/public'
+			}
+		}
+	},
 }
--- docs/old/document.md
+++ docs/old/document.md
@@ -1,6 +1,4 @@
-# demo
+# document
 
 <!-- 默认静态资源路径为**/public -->
 ![海贼王](~@/image/haizei.jpg)
-
-<!-- ![海贼王](~@fuck/haizei.jpg) -->
